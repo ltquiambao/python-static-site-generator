585e4e07717cbca99eed26ba405450e31911a00c
ltquiambao <lorenzotquiambao@gmail.com>
2020-09-15 18:40:44 -0400

    Completed Module2_3

---
+++ ssg/parsers.py
@@ -0,0 +1,13 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
