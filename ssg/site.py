6c4bb29a1fb86eeb9298b5307735a7f48607b7ff
ltquiambao <lorenzotquiambao@gmail.com>
2020-09-14 23:44:51 -0400

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -8,4 +8,8 @@ class Site:
     def create_dir(self, path):
         destination = self.dest
         relative_to_source = path.relative_to(self.source)
-        directory = '{0}/{1}'.format(destination, relative_to_source)
+        directory = destination / relative_to_source
+        directory.mkdir(parents = True, exist_ok = True)
+
+    def build():
+        self.dest.mkdir(parents = True, exist_ok = True)
